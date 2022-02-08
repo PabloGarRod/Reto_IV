35909dd49bdcad746c8e26872065a5a5d38a2d31
Pablo <pablogarcirodriguez@gmail.com>
2022-02-08 12:19:21 +0100

    modulo main
    En este commit hemos añadido el módulo main.py donde hemos instanciado las clases e implementado loa métodos ejecutables.

--- RETO_IV.py
+++ RETO_IV.py
@@ -1,4 +1,19 @@
-class Serie:
+class Entregable:
+
+    def __init__(self):
+        self.entregado = None
+
+    def entregar(self):
+        self.entregado = True
+
+    def devolver(self):
+        self.entregado = False
+
+    def isEntregado(self):
+        return self.entregado
+
+
+class Serie(Entregable):
     titulo = ""
     numero_de_temporadas = 3
     entregado = False
@@ -40,7 +55,7 @@ class Serie:
         return f"{self.titulo}, {self.numero_de_temporadas}, entregado: {self.entregado}, {self.genero}, {self.creador}"
 
 
-class Videojuego:
+class Videojuego(Entregable):
     titulo = ""
     horas_estimadas = 10
     entregado = False
@@ -79,85 +94,4 @@ class Videojuego:
         self.compañía = compañia
 
     def __str__(self):
-        return f"{self.titulo}, {self.horas_estimadas}, {self.entregado}, entregado: {self.entregado}, {self.genero}, {self.compañía}"
-
-
-class Entregable(Serie, Videojuego):
-
-    def entregar(self):
-        self.entregado = True
-
-    def devolver(self):
-        self.entregado = False
-
-    def isEntregado(self):
-        return self.entregado
-
-
-serie1 = Entregable("Berserk", 5, False, "Seinen", "Kintaro Miura")
-serie2 = Entregable("SNK", 4, False, "Acción", "Hajime Isayama")
-serie3 = Entregable("Rick y Morty", 4, True, "comedia", "Dan Harmon")
-serie4 = Entregable("Dragon Ball", 6, True, "Shonen", "Akira Toriyama")
-serie5 = Entregable("Juego de tronos", 8, True, "fantasía", "George R.R. Martin")
-
-lista_series = [serie1, serie2, serie3, serie4, serie5]
-
-videojuego1 = Entregable("League of Legends", 15, True, "Moba", "Riot Games")
-videojuego2 = Entregable("World of Warcraft", 20, False, "MMORPG", "Blizzard")
-videojuego3 = Entregable("Call of Duty", 16, False, "Shooter", "Activision")
-videojuego4 = Entregable("Super Mario", 68, False, "Plataformas", "Nintendo")
-videojuego5 = Entregable("Fortnite", 26, False, "Battle Royale", "Epic Games")
-
-lista_videojuegos = [videojuego1, videojuego2, videojuego3, videojuego4, videojuego5]
-
-
-def contar_series_entregadas(series):
-    num_series = 0
-    print("Las series entregadas son: ")
-    for serie in series:
-        if serie.entregado:
-            num_series += 1
-            print(serie.titulo)
-    print(f"En total se han entregado: {num_series} series")
-
-
-def contar_videojuegos_entregados(videojuegos):
-    num_videojuegos = 0
-    print("Los videojuegos entregados son: ")
-    for juego in videojuegos:
-        if juego.entregado:
-            num_videojuegos += 1
-            print(juego.titulo)
-    print(f"En total se han entregado: {num_videojuegos} videojuegos")
-
-
-def serie_mas_temporadas(series):
-    temporadas = 0
-    serie_mas = None
-    for serie in series:
-        if serie.numero_de_temporadas > temporadas:
-            temporadas = serie.numero_de_temporadas
-            serie_mas = serie
-    return serie_mas
-
-def videojuego_mas_horas(videojuegos):
-    horas = 0
-    videojuego_mas = None
-    for videojuego in videojuegos:
-        if videojuego.horas_estimadas > horas:
-            horas = videojuego.horas_estimadas
-            videojuego_mas = videojuego
-    return videojuego_mas
-
-
-
-
-serie1.entregar()
-serie4.entregar()
-videojuego5.entregar()
-videojuego2.entregar()
-
-contar_series_entregadas(lista_series)
-contar_videojuegos_entregados(lista_videojuegos)
-print(serie_mas_temporadas(lista_series))
-print(videojuego_mas_horas(lista_videojuegos))
+        return f"{self.titulo}, {self.horas_estimadas}, {self.entregado}, entregado: {self.entregado}, {self.genero}, {self.compañía} "
--- main.py
+++ main.py
@@ -1,16 +1,71 @@
-# This is a sample Python script.
-
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from RETO_IV import Serie, Videojuego
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def contar_series_entregadas(series):
+    num_series = 0
+    print("Las series entregadas son: ")
+    for serie in series:
+        if serie.entregado:
+            num_series += 1
+            print(serie.titulo)
+    print(f"En total se han entregado: {num_series} series")
 
 
-# Press the green button in the gutter to run the script.
+def contar_videojuegos_entregados(videojuegos):
+    num_videojuegos = 0
+    print("Los videojuegos entregados son: ")
+    for juego in videojuegos:
+        if juego.entregado:
+            num_videojuegos += 1
+            print(juego.titulo)
+    print(f"En total se han entregado: {num_videojuegos} videojuegos")
+
+
+def serie_mas_temporadas(series):
+    temporadas = 0
+    serie_mas = None
+    for serie in series:
+        if serie.numero_de_temporadas > temporadas:
+            temporadas = serie.numero_de_temporadas
+            serie_mas = serie
+    return serie_mas
+
+
+def videojuego_mas_horas(videojuegos):
+    horas = 0
+    videojuego_mas = None
+    for videojuego in videojuegos:
+        if videojuego.horas_estimadas > horas:
+            horas = videojuego.horas_estimadas
+            videojuego_mas = videojuego
+    return videojuego_mas
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    serie1 = Serie("Berserk", 5, False, "Seinen", "Kintaro Miura")
+    serie2 = Serie("SNK", 4, False, "Acción", "Hajime Isayama")
+    serie3 = Serie("Rick y Morty", 4, True, "comedia", "Dan Harmon")
+    serie4 = Serie("Dragon Ball", 6, True, "Shonen", "Akira Toriyama")
+    serie5 = Serie("Juego de tronos", 8, True, "fantasía", "George R.R. Martin")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    lista_series = [serie1, serie2, serie3, serie4, serie5]
+
+    videojuego1 = Videojuego("League of Legends", 15, True, "Moba", "Riot Games")
+    videojuego2 = Videojuego("World of Warcraft", 20, False, "MMORPG", "Blizzard")
+    videojuego3 = Videojuego("Call of Duty", 16, False, "Shooter", "Activision")
+    videojuego4 = Videojuego("Super Mario", 68, False, "Plataformas", "Nintendo")
+    videojuego5 = Videojuego("Fortnite", 26, False, "Battle Royale", "Epic Games")
+
+    lista_videojuegos = [videojuego1, videojuego2, videojuego3, videojuego4, videojuego5]
+
+    serie1.entregar()
+    serie4.entregar()
+    videojuego5.entregar()
+    videojuego2.entregar()
+
+    contar_series_entregadas(lista_series)
+    contar_videojuegos_entregados(lista_videojuegos)
+    print("Información sobre la serie con más temporadas: ")
+    print(serie_mas_temporadas(lista_series))
+    print("Información sobre el videojuego con más horas: ")
+    print(videojuego_mas_horas(lista_videojuegos))
